7290caa11162477bce6ea06f964cd4f16a6b13ad
Sinh <18521338@gm.uit.edu.vn>
2019-12-09 17:13:08 +0700

    Thêm thuộc tính bàn cho hóa đơn

--- Quan_Li_Cua_Hang/DTO_QuanLi/DTO_HoaDon.cs
+++ Quan_Li_Cua_Hang/DTO_QuanLi/DTO_HoaDon.cs
@@ -12,28 +12,32 @@ namespace DTO_QuanLi
         private DateTime ngayhd;
         private float trigia;
         private int trangthai;
+        private int soban;
 
         public int Mahd { get => mahd; set => mahd = value; }
         public DateTime Ngayhd { get => ngayhd; set => ngayhd = value; }
         public float Trigia { get => trigia; set => trigia = value; }
         public int Trangthai { get => trangthai; set => trangthai = value; }
+        public int Soban { get => soban; set => soban = value; }
 
         public DTO_HoaDon()
         {
 
         }
-        public DTO_HoaDon(int ma, DateTime ngay, float gia, int t_thai)
+        public DTO_HoaDon(int ma, DateTime ngay, float gia, int t_thai,int s_ban)
         {
             this.Mahd = ma;
             this.Ngayhd = ngay;
             this.Trigia = gia;
             this.Trangthai = t_thai;
+            this.Soban = s_ban;
         }
-        public DTO_HoaDon(DateTime ngay, float gia, int t_thai)
+        public DTO_HoaDon(DateTime ngay, float gia, int t_thai,int s_ban)
         {
             this.Ngayhd = ngay;
             this.Trigia = gia;
             this.Trangthai = t_thai;
+            this.Soban = s_ban;
         }
 
     }
