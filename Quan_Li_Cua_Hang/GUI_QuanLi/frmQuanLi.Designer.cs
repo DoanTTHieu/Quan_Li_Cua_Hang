89922ab3ba5680191cfd1e98e7454ac02ee16114
Sinh <18521338@gm.uit.edu.vn>
2019-12-10 15:16:52 +0700

    chỉnh size

--- Quan_Li_Cua_Hang/GUI_QuanLi/frmQuanLi.Designer.cs
+++ Quan_Li_Cua_Hang/GUI_QuanLi/frmQuanLi.Designer.cs
@@ -35,9 +35,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 650);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmQuanLi";
-            this.Text = "frmQuanLi";
+            this.Text = "Quản Lí";
             this.ResumeLayout(false);
 
         }
